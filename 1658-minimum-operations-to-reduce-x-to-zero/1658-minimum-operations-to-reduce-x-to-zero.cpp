3918663a6c90108a4160dd1f491b0351653b7f09
priyanshu847700 <priyanshu847700@gmail.com>
2026-09-26 11:36:39 +0530

    Time: 7 ms (35.91%), Space: 102.3 MB (63.63%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,37 +2,35 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int sum=0;
-        unordered_map<int,int> mp;
-
-        mp[0]=-1;
-
+        
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            mp[sum]=i;
         }
 
         if(sum<x){
             return -1;
         }
 
-        int longest_subarray_sum=sum-x;
-        int longest_subarray_size=INT_MIN;
-
+        int target_sum=sum-x;
+        int l=0,r=0,maxlen=INT_MIN;
         sum=0;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
+        while(r<nums.size()){
+            sum+=nums[r];
 
-            int findsum=sum-longest_subarray_sum;
-
-            if(mp.find(findsum) != mp.end()){
-                int idx=mp[findsum];
-
-                longest_subarray_size=max(longest_subarray_size,i-idx);
+            while(sum>target_sum){
+                sum-=nums[l];
+                l++;
             }
+            
+            if(sum == target_sum){
+                maxlen=max(maxlen,r-l+1);
+            }
+            
+            r++;
         }
-        if(longest_subarray_size==INT_MIN){
-            return -1;
+        if(maxlen==INT_MIN){
+            return -1; 
         }
-        return (nums.size()-longest_subarray_size);
+        return nums.size()-maxlen;
     }
 };
